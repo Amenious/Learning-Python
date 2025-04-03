5ee5e1f011a0112b8352a7c3dec2de95ab26eb8e
Amenious <brandonj.fredericksen@gmail.com>
2025-04-02 22:32:52 -0400

    Create basics.py
    Started new file as a playground, with basic syntax with notes

---
+++ Data Types/basics.py
@@ -0,0 +1,15 @@
+#These are variables
+#Integer
+x = 10
+#String
+y = "Hello"
+#Float
+z = 10.1
+
+#This is a list
+student_grades = [9.1, 8.8, 7.5]
+
+#This is a range
+fun_range = list(range(1, 9, 2))
+
+print(fun_range)
