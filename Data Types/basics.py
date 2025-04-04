dae0d2d07e257a4bb437507533c6820865066e05
Amenious <brandonj.fredericksen@gmail.com>
2025-04-03 22:32:32 -0400

    Update basics.py
    Minor updates to learning, dictionaries and tuples. Internet issues so committing and stopping.

--- Data Types/basics.py
+++ Data Types/basics.py
@@ -6,8 +6,12 @@ y = "Hello"
 #Float
 z = 10.1
 
-#This is a list
+#This is a list. This is mutable
 student_grades = [9.1, 8.8, 7.5]
+#This is a Dictionary
+student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+#This is a tuple. This is immutable
+monday_temperatures = (1, 4, 5)
 
 #This is a range
 fun_range = list(range(1, 9, 2))
@@ -15,7 +19,7 @@ fun_range = list(range(1, 9, 2))
 #print(fun_range)
 
 #Average of student grades
-mysum = sum(student_grades)
+mysum = sum(student_grades.values())
 length = len(student_grades)
 mean = mysum / length
 
