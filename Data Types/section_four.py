ccc029d896251c4665a816c7c93648b2adfe6241
Amenious <brandonj.fredericksen@gmail.com>
2025-04-08 22:50:04 -0400

    Update section_four.py
    Finished section 4, added short line of code for indexing exercise with dictionary

--- Data Types/section_four.py
+++ Data Types/section_four.py
@@ -13,3 +13,8 @@ weekends = ['saturday', 'sunday']
 
 weekdays.append(weekends[0])
 print(weekdays)
+
+tuesday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+
+data = {"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]}
+print(data['b'][2])
