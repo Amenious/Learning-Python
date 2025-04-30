f36a17e5827fe7affab4d3af20e918ce34c7229e
Amenious <brandonj.fredericksen@gmail.com>
2025-04-29 22:35:06 -0400

    added b, finished 7
    Finished section 7 and added a quick script to demonstrate breaks in while loops

--- section_seven.py
+++ section_seven.py
@@ -5,3 +5,20 @@ for temperature in monday_temperatures:
 
 for letter in 'hello':
     print(letter)
+
+#Accessing dictionaries in loops
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+for grades in student_grades.values():
+    print(grades)
+
+phonebook = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+
+for key, value in phonebook.items():
+    print(f"{key}: {value}")
+
+#While loops
+username = ""
+
+while username != "pypy":
+    username = input("Enter username: ")
